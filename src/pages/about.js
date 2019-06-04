3482cc6b447ad8bf08939676bb963fba106f4237
HsuWaiWaiTun <snow.swwt@gmail.com>
2019-06-04 11:02:23 +0800

    tutorial-1

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Link} from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+    <div>
+        <Link to="/">Home</Link>
+        <Header headerText="About US!!"/>
+        <p>Reference</p>
+    </div>
+)
